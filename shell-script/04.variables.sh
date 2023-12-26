1c92c0efdf5c220facb6fb623c667db9de21f861
rajeshkonathala <github.com/Rajeshkonathala/shell.git>
2023-12-26 18:29:01 +0530

    added 2nd script

---
+++ shell-script/04.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo $PERSON1: "hello $PERSON2, Good morning"
+echo $PERSON2: "Hai $PERSON1, Very good morning"
+echo $PERSON1: "How are you $PERSON2"
+echo $PERSON2: "iam Good $PERSON1, How are you."
