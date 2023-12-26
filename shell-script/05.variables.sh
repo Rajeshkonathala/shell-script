4e9fed8d13988acf53a3c0e8b9dccac918877528
rajeshkonathala <github.com/Rajeshkonathala/shell.git>
2023-12-26 18:58:59 +0530

    added 2nd script

--- shell-script/05.variables.sh
+++ shell-script/05.variables.sh
@@ -4,4 +4,10 @@ echo "please enter your username: : "
 
 read -s USERNAME #the value entered above will be automatically attached to  USERNAME variable
 
-echo "username is: $USERNAME"
+echo "username is: $USERNAME" #im printing for validation, you should not echo username and passwords in scripts
+
+echo "please enter your password: : "
+
+read -s PASSWORD
+
+echo "password is: $PASSWORD"
