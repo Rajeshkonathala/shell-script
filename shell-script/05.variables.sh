bc971f05629ed3e5e99f2c4cada61af61cc2860c
rajeshkonathala <github.com/Rajeshkonathala/shell.git>
2023-12-26 18:44:30 +0530

    added 2nd script

--- shell-script/05.variables.sh
+++ shell-script/05.variables.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-USERNAME=$1
-PASSWORD=$2
+echo "please enter your username"
 
-echo "username is: $USERNAME, password is: $PASSWORD"
+read USERNAME #the value entered above will be automatically attached to  USERNAME variable
+
+echo "username is: $USERNAME
