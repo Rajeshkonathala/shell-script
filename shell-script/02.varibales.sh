e7ee33409b8290c891ee5799e9ce12bcdd01d9e6
rajeshkonathala <github.com/Rajeshkonathala/shell.git>
2023-12-26 17:06:10 +0530

    added 2nd script

--- shell-script/02.varibales.sh
+++ shell-script/02.varibales.sh
@@ -2,7 +2,7 @@
 
 PERSON1=Ramesh
 PERSON2=Suresh
-echo PERSON1: "hello $PERSON2, Good morning"
-echo ERSON2: "Hai $PERSON1, Very good morning"
-echo PERSON1: "How are you $PERSON2"
-echo PERSON2: "iam Good $PERSON1, How are you."
+echo $PERSON1: "hello $PERSON2, Good morning"
+echo $PERSON2: "Hai $PERSON1, Very good morning"
+echo $PERSON1: "How are you $PERSON2"
+echo $PERSON2: "iam Good $PERSON1, How are you."
