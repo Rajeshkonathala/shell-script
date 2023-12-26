24cc2877f1729bb494373013597bc60378c5dfbf
rajeshkonathala <github.com/Rajeshkonathala/shell.git>
2023-12-26 16:52:32 +0530

    added 2nd script

---
+++ shell-script/02.varibales.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo Ramesh: "hello Suresh, Good morning"
+echo Suresh: "Hai Ramesh, Very good morning"
+echo Ramesh: "How are you Suresh"
+echo Suresh: "iam Good Ramesh, How are you."
