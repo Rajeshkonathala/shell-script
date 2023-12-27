5310e53767a873678d52a3adbae8479a5ef0bbd2
rajeshkonathala <github.com/Rajeshkonathala/shell.git>
2023-12-27 14:21:57 +0530

    again trying

--- shell-script/08.conditions.sh
+++ shell-script/08.conditions.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 NUMBER=$1
-
-if [ $Number -gt 100 ]
+if [ "$NUMBER" -gt 100 ]
 then
-    echo "Give number $Number ig greater than 100"
+    echo "Give number $NUMBER is greater than 100"
 else
-    echo "Give number $Number is not greater than 100"
+    echo "Give number $NUMBER is not greater than 100"
 fi
