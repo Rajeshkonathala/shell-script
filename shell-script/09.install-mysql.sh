afbc3355e56a2342f76c7be021c75a891ca0130d
rajeshkonathala <github.com/Rajeshkonathala/shell.git>
2023-12-27 17:09:32 +0530

    again trying

--- shell-script/09.install-mysql.sh
+++ shell-script/09.install-mysql.sh
@@ -4,6 +4,7 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
+    exit 1 #if other than 0
 else
     echo "you are root user"
 fi #fi means reverse of if, indication of condition end
