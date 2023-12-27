2c51e8230cf05b0f9f59c6f446a7ccd99afc660e
rajeshkonathala <github.com/Rajeshkonathala/shell.git>
2023-12-27 17:16:11 +0530

    again trying

--- shell-script/09.install-mysql.sh
+++ shell-script/09.install-mysql.sh
@@ -4,9 +4,15 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
-    exit 1 #if other than 0
+    exit 1 #you can give other than 0
 else
     echo "you are root user"
 fi #fi means reverse of if, indication of condition end
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then    
+    echo :ERROR:: Installing MYSQL is failed"
+    exit 1
+fi
