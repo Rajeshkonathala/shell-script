97a5df7454a45ea94e843a4ae5a0bf75f53ea7b1
rajeshkonathala <github.com/Rajeshkonathala/shell.git>
2023-12-28 18:25:24 +0530

    Finally little bit

--- shell-script/11.logs.sh
+++ shell-script/11.logs.sh
@@ -15,10 +15,10 @@ LOGFILE="/tmp/$0-TIMESTAMP.log"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then    
-        echo -e "ERROR:: $2 ... $R FAILED"
+        echo -e "ERROR:: $2 ... $R FAILED $N"
         exit 1
     else
-        echo -e "$2 ... $G SUCCESS"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
