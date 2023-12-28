38da0edc3163101a94d5885e01c433110ae0b64e
rajeshkonathala <github.com/Rajeshkonathala/shell.git>
2023-12-28 17:27:07 +0530

    Finally little bit

---
+++ shell-script/11.logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+echo "script name: $0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then    
+        echo "ERROR:: $2 ... FAILED"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1 #you can give other than 0
+else
+    echo "you are root user"
+fi #fi means reverse of if, indication of condition end
+
+yum install mysql -y
+
+VALIDATE $? "installing MySQL"
+
+yum install git -y
+
+VALIDATE $? "installing GIT"
