985ae1098791ba00a6bad726ac732d3d4456bf23
rajeshkonathala <github.com/Rajeshkonathala/shell.git>
2023-12-29 13:18:20 +0530

    commited

--- shell-script/08.conditions.sh
+++ shell-script/08.conditions.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 NUMBER=$1
-R="e\[31m"
-G="e\[32m"
-Y="e\[33m"
-N="e\[0m"
 
 if [ "$NUMBER" -gt 100 ]
 then
-    echo -e "Give number $G $NUMBER is greater than 100"
+    echo "Give number $NUMBER is greater than 100"
 else
-    echo -e "Give number $R $NUMBER is not greater than 100"
+    echo "Give number $NUMBER is not greater than 100"
 fi
