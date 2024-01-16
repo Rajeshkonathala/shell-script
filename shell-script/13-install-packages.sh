577c6e812bed1cc6f30ec63966b3e41ce08374a4
rajeshkonathala <github.com/Rajeshkonathala/shell.git>
2024-01-16 08:04:09 +0530

    commited

---
+++ shell-script/13-install-packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ID=[id -ne $0]
+
+if [ $ID -ne 0 ]
+then
+    echo -e "ERROR:: $R Please run this script with root access $N"
+    exit 1 #you can give other than 0
+else
+    echo "you are root user"
+fi #fi means reverse of if, indication of condition end
+
+echo "All args passed: $@"
